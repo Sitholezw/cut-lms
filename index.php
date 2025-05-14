2cc7bf1e82310033083f6b082ae015691e090c69
sitholezw <evidencefelixy11@gmail.com>
2025-05-14 12:19:02 +0200

    Refactor HTML structure and clean up unused code in index.php

--- index.php
+++ index.php
@@ -65,44 +65,11 @@ if (isset($_POST['signin'])) {
 
 </head>
 
-<body  style="background-image: url('assets/images/background.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 100vh; filter: blur(0.1px);">
+<body style="background-image: url('assets/images/background.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 100vh; filter: blur(0.1px);">
     <div class="loader-bg"></div>
     <div class="loader">
         <div class="preloader-wrapper big active">
             <div class="spinner-layer spinner-blue">
-                <div class="circle-clipper left">
-                    <div class="circle"></div>
-                </div>
-                <div class="gap-patch">
-                    <div class="circle"></div>
-                </div>
-                <div class="circle-clipper right">
-                    <div class="circle"></div>
-                </div>
-            </div>
-            <div class="spinner-layer spinner-spinner-teal lighten-1">
-                <div class="circle-clipper left">
-                    <div class="circle"></div>
-                </div>
-                <div class="gap-patch">
-                    <div class="circle"></div>
-                </div>
-                <div class="circle-clipper right">
-                    <div class="circle"></div>
-                </div>
-            </div>
-            <div class="spinner-layer spinner-yellow">
-                <div class="circle-clipper left">
-                    <div class="circle"></div>
-                </div>
-                <div class="gap-patch">
-                    <div class="circle"></div>
-                </div>
-                <div class="circle-clipper right">
-                    <div class="circle"></div>
-                </div>
-            </div>
-            <div class="spinner-layer spinner-green">
                 <div class="circle-clipper left">
                     <div class="circle"></div>
                 </div>
@@ -120,54 +87,25 @@ if (isset($_POST['signin'])) {
             <nav class="#82b1ff blue accent-1">
                 <div class="nav-wrapper row">
                     <section class="material-design-hamburger navigation-toggle">
-                        <a href="#" data-activates="slide-out"
-                            class="button-collapse show-on-large material-design-hamburger__icon">
+                        <a href="#" class="button-collapse show-on-large material-design-hamburger__icon">
                             <span class="material-design-hamburger__layer"></span>
                         </a>
                     </section>
                     <div class="header-title col s3">
                         <span class="chapter-title">CUT | Leave Management System</span>
                     </div>
-
                 </div>
             </nav>
         </header>
 
-
-        <aside id="slide-out" class="side-nav futuristic fixed">
-            <div class="side-nav-wrapper futuristic-bg" >
-
-            <ul class="sidebar-menu collapsible collapsi`ble-accordion" data-collapsible="accordion">
-                <li>&nbsp;</li>
-                <li class="no-padding"><a class="waves-effect waves-light futuristic-text" href="index.php"><i
-                    class="material-icons futuristic-icon">account_box</i>Staff Login</a></li>
-                <li class="no-padding"><a class="waves-effect waves-light futuristic-text" href="forgot-password.php"><i
-                    class="material-icons futuristic-icon">lock_clock</i>Forgot Password?</a></li>
-
-                <li class="no-padding"><a class="waves-effect waves-light futuristic-text" href="admin/"><i
-                    class="material-icons futuristic-icon">account_box</i>Admin Login</a></li>
-
-            </ul>
-            <!-- 
-            <div class="footer futuristic-footer">
-                <p class="copyright futuristic-text">© <?php echo date("Y"); ?> All Rights Reserved. <br> Designed & Developed
-                </p> By esithole@cut</p> 
-            </div>
-            -->
-            </div>
-        </aside>
-                    
-        </aside> 
         <main class="mn-inner">
             <div class="row">
                 <div class="col s12">
                     <div class="col s12 m6 l6 offset-l2 offset-m3">
-                        <div style="text-align:center"><img src="assets\images\favicon.png"
-                                alt="Logo" width="150px"></div>
+                        <div style="text-align:center"><img src="assets\images\favicon.png" alt="Logo" width="150px"></div>
 
                         <div class="card white darken-1">
-
-                            <div class="card-content ">
+                            <div class="card-content">
                                 <span class="card-title" style="font-size:20px;">Staff Login</span>
                                 <?php if ($msg) { ?>
                                     <div class="card-panel red lighten-4 red-text text-darken-4">
@@ -177,22 +115,33 @@ if (isset($_POST['signin'])) {
                                 <div class="row">
                                     <form class="col s12" name="signin" method="post">
                                         <div class="input-field col s12">
-                                            <input id="username" type="text" name="username" class="validate"
-                                                autocomplete="off" required>
+                                            <input id="username" type="text" name="username" class="validate" autocomplete="off" required>
                                             <label for="username">Email Address</label>
                                         </div>
                                         <div class="input-field col s12">
-                                            <input id="password" type="password" class="validate" name="password"
-                                                autocomplete="off" required>
+                                            <input id="password" type="password" class="validate" name="password" autocomplete="off" required>
                                             <label for="password">Password</label>
                                         </div>
                                         <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
                                         <div class="col s12 m-t-sm">
-
-                                            <input type="submit" name="signin" value="Sign in"
-                                                class="waves-effect waves-light btn indigo m-b-xs" style="width: 100%;">
+                                            <input type="submit" name="signin" value="Sign in" class="waves-effect waves-light btn indigo m-b-xs" style="width: 100%;">
                                         </div>
                                     </form>
+                                </div>
+                                <div class="row" style="margin-top: 20px;">
+                                    <ul class="sidebar-menu">
+                                        <li class="no-padding">
+                                            <a class="waves-effect waves-light" href="forgot-password.php" style="text-decoration:wavy; color: #3f51b5; font-weight: bold;">
+                                                Forgot Password?
+                                            </a>
+                                        </li>
+                                        <!-- Uncomment if Admin Login is needed -->
+                                        <!-- <li class="no-padding">
+                                            <a class="waves-effect waves-light" href="admin/">
+                                                <i class="material-icons">admin_panel_settings</i> Admin Login
+                                            </a>
+                                        </li> -->
+                                    </ul>
                                 </div>
                             </div>
                         </div>
@@ -200,13 +149,11 @@ if (isset($_POST['signin'])) {
                 </div>
             </div>
         </main>
-
     </div>
-    <div class="left-sidebar-hover"></div>
 
-     <!-- Footer -->
-     <footer class="page-footer" style="position: fixed; bottom: 0; width: 100%; z-index: 1000; background-color: #3f51b5; color: white;">
-        <div class="footer-copyright"  style="background-color: #3f51b5;">
+    <!-- Footer -->
+    <footer class="page-footer" style="position:relative; bottom: 0; width: 100%; z-index: 1000; background-color: #3f51b5; color: white;">
+        <div class="footer-copyright" style="background-color: #3f51b5;">
             <div class="container">
                 © 2025 CUT-LMS. All rights reserved.
                 <span class="right">Follow us on 
@@ -223,20 +170,7 @@ if (isset($_POST['signin'])) {
     <script src="assets/plugins/materialize/js/materialize.min.js"></script>
     <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
     <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
-    <script src="assets/plugins/waypoints/jquery.waypoints.min.js"></script>
-    <script src="assets/plugins/counter-up-master/jquery.counterup.min.js"></script>
-    <script src="assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
-    <script src="assets/plugins/chart.js/chart.min.js"></script>
-    <script src="assets/plugins/flot/jquery.flot.min.js"></script>
-    <script src="assets/plugins/flot/jquery.flot.time.min.js"></script>
-    <script src="assets/plugins/flot/jquery.flot.symbol.min.js"></script>
-    <script src="assets/plugins/flot/jquery.flot.resize.min.js"></script>
-    <script src="assets/plugins/flot/jquery.flot.tooltip.min.js"></script>
-    <script src="assets/plugins/curvedlines/curvedLines.js"></script>
-    <script src="assets/plugins/peity/jquery.peity.min.js"></script>
     <script src="assets/js/alpha.min.js"></script>
-    <script src="assets/js/pages/dashboard.js"></script>
-
 </body>
 
 </html>
