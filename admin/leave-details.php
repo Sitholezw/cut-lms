6555e98964615cc4111b15205bae24d270f7f54b
sitholezw <evidencefelixy11@gmail.com>
2025-06-02 09:01:44 +0200

    Refactor leave-details.php to remove inline styles and link to external leave-details.css for improved styling consistency.

--- admin/leave-details.php
+++ admin/leave-details.php
@@ -67,24 +67,8 @@ $msg="Leave updated Successfully";
         <!-- Theme Styles -->
         <link href="../assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
         <link href="../assets/css/custom.css" rel="stylesheet" type="text/css"/>
-<style>
-        .errorWrap {
-    padding: 10px;
-    margin: 0 0 20px 0;
-    background: #fff;
-    border-left: 4px solid #dd3d36;
-    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-}
-.succWrap{
-    padding: 10px;
-    margin: 0 0 20px 0;
-    background: #fff;
-    border-left: 4px solid #5cb85c;
-    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-}
-        </style>
+        <link rel="stylesheet" href="../assets/css/leave-deatails.css">
+
     </head>
     <body>
        <?php include('includes/header.php');?>
